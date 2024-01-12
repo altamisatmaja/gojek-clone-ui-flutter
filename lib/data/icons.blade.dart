5d136297ca666d1d7484b477b89f6791906d319f
altamisatmaja <altamisfattah16@gmail.com>
2024-01-12 22:31:27 +0700

    feat: setup for main app

---
+++ lib/data/icons.blade.dart
@@ -0,0 +1,19 @@
+import 'package:gojekcloneapp/models/gojek_icon.blade.dart';
+import 'package:gojekcloneapp/theme.blade.dart';
+
+List<GojekIcon> gopayIcons = [
+  GojekIcon(icon: 'pay', title: 'Bayar'),
+  GojekIcon(icon: 'topup', title: 'Top Up'),
+  GojekIcon(icon: 'explore', title: 'Eksplore'),
+];
+
+List<GojekIcon> menuIcons = [
+  GojekIcon(icon: 'goride', title: 'Go Ride', color: green2),
+  GojekIcon(icon: 'gocar', title: 'Go Car', color: green2),
+  GojekIcon(icon: 'gofood', title: 'Go Food', color: red),
+  GojekIcon(icon: 'gosend', title: 'Go Send', color: green2),
+  GojekIcon(icon: 'gomart', title: 'Go Mart', color: green2),
+  GojekIcon(icon: 'gopulsa', title: 'Go Pulsa', color: blue2),
+  GojekIcon(icon: 'goclub', title: 'Go Club', color: purple),
+  GojekIcon(icon: 'other', title: 'Other', color: dark4),
+];
