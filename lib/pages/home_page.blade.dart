dabab01a89dab8afa6da1d679eb109781b1a9d88
altamisatmaja <altamisfattah16@gmail.com>
2024-01-14 19:44:28 +0700

    feat: search

--- lib/pages/home_page.blade.dart
+++ lib/pages/home_page.blade.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gojekcloneapp/components/header.blade.dart';
 import 'package:gojekcloneapp/theme.blade.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,43 +9,49 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: green2,
         elevation: 0,
         toolbarHeight: 71,
-        title: Container(
-          padding: const EdgeInsets.all(5),
-          decoration: BoxDecoration(
-              color: green1, borderRadius: BorderRadius.circular(30)),
-          child: Row(
-            children: [
-              Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(100)),
-                child: Text(
-                  'Beranda',
-                  style: semibold14.copyWith(color: green1),
-                ),
-              ),
-              ...['Promo', 'Pesanan', 'Chat'].map((title) => Flexible(
-                fit: FlexFit.loose,
-                child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 14, vertical: 12),
-                    child: Center(
-                      child: Text(
-                        title,
-                        style: semibold14,
+        title: const Header(),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 23, left: 15, right: 15),
+              child: Row(
+                children: [
+                  Flexible(
+                    fit: FlexFit.tight,
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                          color: const Color(0xFFFAFAFA),
+                          borderRadius: BorderRadius.circular(30),
+                          border: Border.all(color: const Color(0xFFE8E8E8))),
+                      child: Row(
+                        children: [
+                          SvgPicture.asset(
+                            'assets/icons/search.svg',
+                            // color: dark1,
+                            width: 20,
+                            height: 20,
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const Text('Cari makanan, layanan, & tujuan  '),
+                        ],
                       ),
-                    )
+                    ),
                   ),
-              )
-              )
-            ],
-          ),
+                  Container()
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
