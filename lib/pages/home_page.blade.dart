db3a06956b17fd2a77b491b38b44344259e2db67
altamisatmaja <altamisfattah16@gmail.com>
2024-01-25 06:24:12 +0700

    feat: akses cepat

--- lib/pages/home_page.blade.dart
+++ lib/pages/home_page.blade.dart
@@ -205,35 +205,33 @@ class HomePage extends StatelessWidget {
                     crossAxisCount: 4,
                     mainAxisSpacing: 2,
                     children: [
-                      ...menuIcons.map((icon) => Container(
-                            child: Column(
-                              children: [
-                                Container(
-                                  height: 40,
-                                  width: 40,
-                                  decoration: BoxDecoration(
-                                      color: icon.icon == 'goclub'
-                                          ? Colors.white
-                                          : icon.color,
-                                      borderRadius: BorderRadius.circular(20)),
-                                  child:
-                                      SvgPicture.asset('icons/${icon.icon}.svg',
-                                          width: 24,
-                                          color: icon.icon == 'goclub'
-                                              ? icon.color
-                                              : icon.icon == 'other'
-                                                  ? dark2
-                                                  : Colors.white),
-                                ),
-                                const SizedBox(
-                                  height: 9,
-                                ),
-                                Text(
-                                  icon.title,
-                                  style: regular12_5.copyWith(color: dark2),
-                                ),
-                              ],
-                            ),
+                      ...menuIcons.map((icon) => Column(
+                            children: [
+                              Container(
+                                height: 40,
+                                width: 40,
+                                decoration: BoxDecoration(
+                                    color: icon.icon == 'goclub'
+                                        ? Colors.white
+                                        : icon.color,
+                                    borderRadius: BorderRadius.circular(20)),
+                                child:
+                                    SvgPicture.asset('icons/${icon.icon}.svg',
+                                        width: 24,
+                                        color: icon.icon == 'goclub'
+                                            ? icon.color
+                                            : icon.icon == 'other'
+                                                ? dark2
+                                                : Colors.white),
+                              ),
+                              const SizedBox(
+                                height: 9,
+                              ),
+                              Text(
+                                icon.title,
+                                style: regular12_5.copyWith(color: dark2),
+                              ),
+                            ],
                           ))
                     ],
                   )),
@@ -301,19 +299,88 @@ class HomePage extends StatelessWidget {
                                       ))
                                 ],
                               )),
-                              const SizedBox(
-                                    width: 25,
-                                  ),
-                              SvgPicture.asset(
-                                'icons/left.svg',
-                                height: 24,
-                                color: dark1,
-                              )
+                          const SizedBox(
+                            width: 25,
+                          ),
+                          SvgPicture.asset(
+                            'icons/left.svg',
+                            height: 24,
+                            color: dark1,
+                          )
                         ],
                       ),
                     )
                   ],
                 ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16, right: 15, left: 15),
+              child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Akses Cepat',
+                      style: bold16.copyWith(color: dark1),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 4),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        border: Border.all(color: const Color(0xFFE8E8E8))),
+                    child: Column(
+                      children: [
+                        ...[
+                          'Pintu masuk motor MNC Land',
+                          'Pintu masuk motor MNC Land'
+                        ].map((text) => Container(
+                              padding: const EdgeInsets.all(16),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    height: 24,
+                                    width: 24,
+                                    decoration: BoxDecoration(
+                                        color: green2,
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
+                                    child: SvgPicture.asset(
+                                      'icons/goride.svg',
+                                      color: Colors.white,
+                                      width: 24,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 12,
+                                  ),
+                                  Flexible(
+                                    fit: FlexFit.tight,
+                                    child: Text(
+                                      text,
+                                      style: regular14.copyWith(color: dark1),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 25,
+                                  ),
+                                  SvgPicture.asset(
+                                    'icons/left.svg',
+                                    height: 24,
+                                    color: dark1,
+                                  )
+                                ],
+                              ),
+                            ))
+                      ],
+                    ),
+                  )
+                ],
               ),
             )
           ],
