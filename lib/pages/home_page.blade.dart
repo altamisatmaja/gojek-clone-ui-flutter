855b77497d7c9da7e5d48f4b0de13d4f146a3c53
altamisatmaja <altamisfattah16@gmail.com>
2024-01-25 05:42:06 +0700

    feat: goclub

--- lib/pages/home_page.blade.dart
+++ lib/pages/home_page.blade.dart
@@ -237,6 +237,84 @@ class HomePage extends StatelessWidget {
                           ))
                     ],
                   )),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 19, left: 15, right: 15),
+              child: Container(
+                height: 60,
+                width: double.infinity,
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: [Color(0xFFEAF3F6), Colors.white]),
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(color: const Color(0xFFE8E8E8))),
+                child: Stack(
+                  children: [
+                    Positioned(
+                        left: 8,
+                        top: 4,
+                        bottom: 4,
+                        child: SvgPicture.asset('icons/dots.svg')),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 12),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Stack(
+                            children: [
+                              SvgPicture.asset(
+                                'icons/star.svg',
+                                height: 40,
+                              )
+                            ],
+                          ),
+                          const SizedBox(
+                            width: 16,
+                          ),
+                          Flexible(
+                              fit: FlexFit.tight,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '117 XP Lagi ada Harta Karun',
+                                    style: semibold14.copyWith(color: dark1),
+                                  ),
+                                  const SizedBox(
+                                    height: 8,
+                                  ),
+                                  Container(
+                                      width: double.infinity,
+                                      clipBehavior: Clip.hardEdge,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(2),
+                                        color: dark3,
+                                      ),
+                                      child: LinearProgressIndicator(
+                                        backgroundColor: dark3,
+                                        color: green1,
+                                        value: .8,
+                                      ))
+                                ],
+                              )),
+                              const SizedBox(
+                                    width: 25,
+                                  ),
+                              SvgPicture.asset(
+                                'icons/left.svg',
+                                height: 24,
+                                color: dark1,
+                              )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
             )
           ],
         ),
